e58af5746933d937a703816f8252065ee5d8bf92
mcavalieri <maurizio.cavalieri@utoronto.ca>
2016-03-15 19:33:11 -0400

    updated footer test 1

--- footer.php
+++ footer.php
@@ -14,15 +14,35 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class ="container">
+				<!--footer widgets -->
+				<div id="footer-widgets">
+					<php if(is_active_sidebar('footer')):?>
+						<aside id="widget-foot" class="widget-foot">
+					<?php dynamic_sidebar( 'footer' ); ?>
+						</aside>
+					<?php endif; ?>
+			</div>
+
+
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'burger-artist-master' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'burger-artist-master' ), 'WordPress' ); ?></a>
+			<a href="<?php echo 
+			esc_url( __( 'https://wordpress.org/', 'burger-artist-master' ) ); ?>">
+				<?php printf( esc_html__( 'Proudly powered by %s', 'burger-artist-master' ), 'WordPress' ); ?>
+			</a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'burger-artist-master' ), 'burger-artist-master', '<a href="http://google.com" rel="designer">Maurizio & ZiXin</a>' ); ?>
 		</div><!-- .site-info -->
+
+		<!-- #footermenu -->
+			<div id="footermenu">
+				<?php wp_nav_menu( array( 
+					'theme_location' => 'secondary',
+					'menu_class' => 'foot-menu' ) ); ?>
+			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
