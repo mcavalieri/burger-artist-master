64652cde8b23454d0d259cdc34ac5ec6cdde3449
justadream008 <WhiteRichBeauty@utmuser51-1.wireless.utoronto.ca>
2016-03-16 15:26:42 -0400

    responsive footer 1

--- footer.php
+++ footer.php
@@ -14,17 +14,18 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-			
+		<div class="container">	
+			<!--footer widgets starts-->
 			<div class="footer-widgets">
 				<?php if ( is_active_sidebar( 'footer-widget-area' ) ) : ?>
 	                	
 	                    	<?php dynamic_sidebar( 'footer-widget-area' ); ?>
 	                
 	            <?php endif; ?>
-			</div>
+			</div><!--footer widget ends-->
 		
 		
-		
+				<!--site info starts-->
 				<div class="site-info">
 					<a href="<?php echo 
 					esc_url( __( 'https://wordpress.org/', 'burger-artist-master' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'burger-artist-master' ), 'WordPress' ); ?>
