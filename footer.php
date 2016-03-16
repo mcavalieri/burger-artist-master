f876c56d872728cefd3140046dd12e7271dbcf21
mcavalieri <maurizio.cavalieri@utoronto.ca>
2016-03-15 20:42:24 -0400

    update

--- footer.php
+++ footer.php
@@ -15,22 +15,13 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class ="container">
-				<!--footer widgets -->
-				<div id="footer-widgets">
-					<php if(is_active_sidebar('footer')):?>
-						<aside id="widget-foot" class="widget-foot">
-					<?php dynamic_sidebar( 'footer' ); ?>
-						</aside>
-					<?php endif; ?>
-				</div>
-
-
 				<div class="site-info">
 					<a href="<?php echo 
 					esc_url( __( 'https://wordpress.org/', 'burger-artist-master' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'burger-artist-master' ), 'WordPress' ); ?>
 					</a>
 				<span class="sep"> | </span>
-					<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'burger-artist-master' ), 'burger-artist-master', '<a href="http://google.com" rel="designer">Maurizio & ZiXin</a>' ); ?>
+					<?php printf( 
+					esc_html__( 'Theme: %1$s by %2$s.', 'burger-artist-master' ), 'burger-artist-master', '<a href="http://google.com" rel="designer">Maurizio & ZiXin</a>' ); ?>
 				</div><!-- .site-info -->
 		</div>
 
