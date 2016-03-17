c0270fb5b4ff2520e75490994f9e9fced7cf7458
mcavalieri <maursader@me.com>
2016-03-17 04:40:09 -0400

    Removed post excerpt..

--- template-parts/content.php
+++ template-parts/content.php
@@ -14,7 +14,7 @@
 			if ( is_single() ) {
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			} else { ?> 
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('menuPics'); ?></a>
+				<br/><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('menuPics'); ?></a>
 		<?php }
 
 		if ( 'post' === get_post_type() ) : ?>
@@ -26,18 +26,7 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'burger-artist-master' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'burger-artist-master' ),
-				'after'  => '</div>',
-			) );
-		?>
+		
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
