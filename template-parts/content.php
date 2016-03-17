e9f284a79652cf5ec1a2cc458d5a3bb7c0b56cb4
mcavalieri <maursader@me.com>
2016-03-17 07:09:11 -0400

    Fixed thumbnails, updated CSS, updated contents..
    Added new bg wallpaper as well as menu item bg..

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,12 +9,12 @@
 
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+
 		<?php
 			if ( is_single() ) {
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			} else { ?> 
-				<br/><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('menuPics'); ?></a>
+				<br/><div id="burger-menu"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('menuPics'); ?></a></div>
 		<?php }
 
 		if ( 'post' === get_post_type() ) : ?>
@@ -23,11 +23,7 @@
 		</div><!-- .entry-meta -->
 		<?php
 		endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		
-	</div><!-- .entry-content -->
+	
 
 	<footer class="entry-footer">
 		<?php burger_artist_master_entry_footer(); ?>
