56924e32cc334296a6c189ead62352fd9ccd3d74
mcavalieri <maursader@me.com>
2016-03-17 13:29:53 -0400

    Added documentation to Options.php

--- inc/options.php
+++ inc/options.php
@@ -2,9 +2,9 @@
 
 // Source: Course Slides & WP Codex 
 
-function theme_settings_page(){
- ?>
-	    <div class="wrap">
+function theme_settings_page(){ // Function to save/process data collected below (save changes button) 
+ ?> 
+	    <div class="wrap">  <!-- Div created for separate item menu --> 
 	    <h1>Theme Options</h1>
 	    <form method="post" action="options.php">
 	        <?php
@@ -16,20 +16,21 @@ function theme_settings_page(){
 		</div>
 	<?php
 }
+// First theme customization, create function to call twitter URL.
 function display_twitter_element()
 {
 	?>
     	<input type="text" name="twitter_url" id="twitter_url" value="<?php echo get_option('twitter_url'); ?>" />
     <?php
 }
-
+// Second theme customization, create function to call facebook URL.
 function display_facebook_element()
 {
 	?>
     	<input type="text" name="facebook_url" id="facebook_url" value="<?php echo get_option('facebook_url'); ?>" />
     <?php
 }
-
+// Third theme customization, create function to call yelp URL.
 function display_yelp_element()
 {
 	?>
@@ -37,14 +38,14 @@ function display_yelp_element()
     <?php
 }
 
-function display_theme_panel_fields()
+function display_theme_panel_fields() // Create means for user input, calls previous functions to obtain URLs
 {
 	add_settings_section("section", "Customize The Burger Artist Theme", null, "theme-options");
-	
+	// User fields for data input below
 	add_settings_field("twitter_url", "Twitter URL:", "display_twitter_element", "theme-options", "section");
     add_settings_field("facebook_url", "Facebook Page URL:", "display_facebook_element", "theme-options", "section");
 	add_settings_field("yelp_url", "Yelp Page URL:", "display_yelp_element", "theme-options", "section");
-
+	
     register_setting("section", "twitter_url");
     register_setting("section", "facebook_url");
 	register_setting("section", "yelp_url");
@@ -52,7 +53,7 @@ function display_theme_panel_fields()
 
 add_action("admin_init", "display_theme_panel_fields");
 
-function add_theme_menu_item()
+function add_theme_menu_item() //Create entry for theme options in WP admin dashboard. 
 {
 	add_menu_page("Theme Options", "Theme Options", "manage_options", "theme-options", "theme_settings_page", null, 99);
 }
